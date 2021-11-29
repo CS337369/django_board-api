003757acd4def5ba3cb12bbe5132b0a6c68ce5cc
ChangSoo <ckdtnek123@kakao.com>
2021-11-29 22:13:55 +0900

    주석 지움

--- boardapi_server/boardapi/views.py
+++ boardapi_server/boardapi/views.py
@@ -1,23 +1,17 @@
 from .models import Board, Comment
-from .serializers import CommentSerializer, BoardSerializer, BoardCreateSerializer, BoardUpdateSerializer, BoardDetailSerializer, CommentListSerializer, SignupSerializer
-from .forms import CommentForm
-from django.shortcuts import get_object_or_404, redirect, render
+from .serializers import CommentSerializer, BoardSerializer, CommentListSerializer
+from django.shortcuts import get_object_or_404
 from django.db.models import F
-from rest_framework import viewsets
-from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, ListAPIView
 from rest_framework import generics, status
 from rest_framework.response import Response
-from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from django.http import JsonResponse
 
+# from rest_framework import viewsets
 from rest_framework.decorators import api_view
 
 
-# from requests.api import request
-
-
+############################### Board ###################################
 
 # class BoardViewSet(viewsets.ModelViewSet):
 #     queryset = Board.objects.all()
@@ -28,47 +22,12 @@ from rest_framework.decorators import api_view
 #     # "post": "create",
 # })
 
-# class BoardListView(ListAPIView):
-    # lookup_field = 'no'
-    # queryset = Board.objects.all().order_by('-b_no')
-    # serializer_class = BoardSerializer
-
-# @csrf_exempt
-# @method_decorator(csrf_exempt, name='dispatch')
-# class BoardCreateView(CreateAPIView):
-    # queryset = Board.objects.all()
-    # serializer_class = BoardCreateSerializer
 
 # @csrf_exempt
 class BoardListCreateAPIView(generics.ListCreateAPIView):
-    # lookup_field = 'no'
     queryset = Board.objects.all().order_by('-b_no')
     serializer_class = BoardSerializer
 
-
-# class BoardDetailView(RetrieveAPIView):
-#     queryset = Board.objects.all()
-#     serializer_class = BoardDetailSerializer
-
-#     def get(self, request, *args, **kwargs):
-#         pk = self.kwargs['pk']
-#         Board.objects.filter(pk=pk).update(b_count=F('b_count') + 1)
-#         return super().get(request, *args, **kwargs)
-
-#     def get_context_data(self, **kwargs):
-#         context_data = super().get_context_data(**kwargs)
-#         return context_data
-
-# @csrf_exempt
-# class BoardUpdateView(UpdateAPIView):
-#     queryset = Board.objects.all()
-#     serializer_class = BoardUpdateSerializer
-
-
-# @csrf_exempt
-# class BoardDeleteView(DestroyAPIView):
-#     queryset = Board.objects.all()
-#     serializer_class = BoardSerializer
 
 class BoardView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Board.objects.all()
@@ -84,16 +43,6 @@ class BoardView(generics.RetrieveUpdateDestroyAPIView):
         return context_data
 
 ############################### Comment ##################################
-
-# class CommentListView(ListAPIView):
-#     model = Comment
-#     queryset = Comment.objects.all().order_by('-id')
-
-#     def get(self, request, board_pk):
-#         board = get_object_or_404(Board, pk=board_pk)
-#         comments = board.comments.all()
-#         serializer = CommentListSerializer(comments, many=True)
-#         return Response(serializer.data)
 
 
 class CommentListCreateView(generics.ListCreateAPIView):
@@ -117,42 +66,7 @@ class CommentListCreateView(generics.ListCreateAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-# class CommentCreateView(CreateAPIView):
-#     model = Comment
-#     queryset = Comment.objects.all()
-#     serializer_class = CommentSerializer
-
-#     def post(self, request, board_pk):
-#         board = get_object_or_404(Board, pk=board_pk)
-#         serializer = CommentSerializer(data=request.data) 
-        
-#         if serializer.is_valid(raise_exception=True):
-#             serializer.save(Board=board)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
 class CommentUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     model = Comment
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-
-
-
-############### account ###################
-
-# def loginview(request):
-#     return render(request, 'loginview.html')
-
-# def signup(request):
-#     serializer_class = SignupSerializer
-
-    # id = request.POST.get('id')
-    # pw1 = request.POST.get('pw1')
-    # pw2 = request.POST.get('pw2')
-
-    # if pw1 == pw2:
-        # User.object.save()
-
-    # if pw1 != pw2:
-    #     msg = message.error('비번틀림')
-    #     return msg
--- django-board/board/urls.py
+++ django-board/board/urls.py
@@ -4,24 +4,11 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('board', views.board.as_view(), name="board"),
-    path('board_write', views.board_write, name="board_write"),
+    path('board/', views.board.as_view(), name="board"),
+    path('board_write/', views.board_write, name="board_write"),
     path('board_insert', views.board_insert.as_view(), name="board_insert"),
-    path('board_view', views.board_view.as_view(), name="board_view"),
-    path('board_edit', views.board_edit, name="board_edit"),
-    path('board_update', views.board_update, name="board_update"),
-    path('board_delete', views.board_delete, name="board_delete"),
-    # path('board_writeajax', views.board_writeajax, name="board_writeajax"),
-    # path('board_insertajax', views.board_insertajax, name="board_insertajax"),
-
-    # path('board_ajax', views.board_ajax, name="board_ajax"),
-    # path('board_deleteajax', views.board_deleteajax, name="board_deleteajax"),
-
-    # path('boardapi', views.boardapi, name="boardapi"),
-    # path('boardapi_view', views.boardapi_view, name="boardapi_view"),
-    # path('boardapi_edit', views.boardapi_edit, name="boardapi_edit"),
-    # path('boardapi_update', views.boardapi_update, name="boardapi_update"),
-    # path('boardapi_write', views.boardapi_write, name="boardapi_write"),
-    # path('boardapi_insert', views.boardapi_insert, name="boardapi_insert"),
-    # path('boardapi_deleteajax', views.boardapi_deleteajax, name="boardapi_deleteajax"),
+    path('board_view/', views.board_view.as_view(), name="board_view"),
+    path('board_edit/', views.board_edit, name="board_edit"),
+    path('board_update/', views.board_update, name="board_update"),
+    path('board_delete/', views.board_delete, name="board_delete")
 ]
